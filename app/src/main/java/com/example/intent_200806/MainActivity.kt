1e3c6ef91ec494acbcc5a4a8e029c51fdc2ba0b9
박새 <yannn7979@gmail.com>
2020-08-06 22:15:16 +0900

    닉네임을 받기 위해 이동하는 코드 추가

--- app/src/main/java/com/example/intent_200806/MainActivity.kt
+++ app/src/main/java/com/example/intent_200806/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    1000의 의미가 닉네임 변경하는 요청이라는걸 인식하기 어렵다
+//    그렇다면, 별개의 멤버변수로 1000을 담아서, 이름을 보고 알 수 있도록 처리하면 되겠지. 변수명 보면 알아차릴 수 있게
+    val REQ_FOR_NICKNAME = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,5 +33,18 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(myIntent)
        }
+
+        changeNickNameBtn.setOnClickListener {
+
+//            닉네임 변경화면으로 이동 => 새 닉네임 받으러 이동
+            val myIntent = Intent(this, EditNickNameActivity::class.java)
+
+//            단순 이동이 아니라, 결과 받으러 (출력하러) 이동 => 어떤 결과를 받으러 가는건지 명시 해줘야 함
+//            고유 숫자를 임의로 지정해서 명시 하면 됨 (1000번이 닉네임 가지러 가는거다 ~라고 내 맘대로 1000으로 지정함)
+//            근데 그러면 1000만 봤을때 무슨 의미인지 모르니까 맨 위로 올라가서 멤버변수에 담아. 그럼 변수명 보고 의미 확인 가능해. 가독성 향상
+//            멤버변수로 숫자 다룰때는 다 대문자에 언더바를 이용해서 이름 지으면 됨됨
+            startActivityForResult(myInent, REQ_FOR_NICKNAME)
+
+        }
     }
 }
