42844ba04a822eb07ba06377399e12ff36c30d85
박새 <yannn7979@gmail.com>
2020-08-06 22:20:42 +0900

    일단 완료 누르면 돌아가게 처리. xml에 okBtn만들어줘야함 ㅜ

--- app/src/main/java/com/example/intent_200806/EditNickNameActivity.kt
+++ app/src/main/java/com/example/intent_200806/EditNickNameActivity.kt
@@ -7,5 +7,17 @@ class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+        okBtn.setOnClickListener{
+
+//            완료버튼 눌렀다 => 정말 데이터 들고 돌아가겠다 (확정) =>복귀 :finish()
+//            백버튼 눌러서 종료 하는 것과 구분 필요
+
+
+            finish()
+        }
+
+
+
     }
 }
