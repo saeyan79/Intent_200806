7a86a2c3c308b362f24158df2eee7cdd037ef2a1
박새 <yannn7979@gmail.com>
2020-08-06 21:55:45 +0900

    second 액티비티에서 첨부 된 내용을 받아서 텍스트뷰에 반영

--- app/src/main/java/com/example/intent_200806/SecondActivity.kt
+++ app/src/main/java/com/example/intent_200806/SecondActivity.kt
@@ -2,10 +2,17 @@ package com.example.intent_200806
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+//        세컨드 액티비티에 들어오자마자 => 바로 첨부 된 값을 받아서 => 텍스트뷰에 반영시키기
+        val receivedMessage = intent.getStringExtra("message")
+
+        receivedMessageTxt.text = receivedMessage
+
     }
 }
