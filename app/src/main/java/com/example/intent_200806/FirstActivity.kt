724edef777f00a6004efa7c64b55d5503cc5e0b4
박새 <yannn7979@gmail.com>
2020-08-06 21:34:03 +0900

    finish() 활용해서 현재 화면 종료 => 이전 화면으로 복귀

--- app/src/main/java/com/example/intent_200806/FirstActivity.kt
+++ app/src/main/java/com/example/intent_200806/FirstActivity.kt
@@ -1,11 +1,24 @@
 package com.example.intent_200806
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        moveToMainBtn.setOnClickListener {
+//            Main로 이동하기 => Intent
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+//            인텐트를 만들면 새 액티비티가 추가 되는 것이다. 종료가 아니라! 우리가 원하는것은 종료
+
+//            현재 액티비티 종료 코드는 finish()
+            finish()
+        }
     }
 }
