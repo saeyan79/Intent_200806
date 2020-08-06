cbf3c629fbfb0482965446e39f977a63999e8f0f
박새 <yannn7979@gmail.com>
2020-08-06 21:46:12 +0900

    메인에서 입력받은 내용을 가지고 second 화면으로 이동

--- app/src/main/java/com/example/intent_200806/MainActivity.kt
+++ app/src/main/java/com/example/intent_200806/MainActivity.kt
@@ -15,5 +15,18 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this, FirstActivity::class.java)
             startActivity(myIntent)
         }
+
+        moveToSecondBtn.setOnClickListener {
+//            SecondActivity로 => 입력한 내용을 들고 => 이동하기
+            val inputMessage = messageEdt.text.toString()
+
+            val myIntent = Intent(this, SecondActivity::class.java)
+
+//            inputMessage를 들고가는데 거기다 message라는 스티커 붙인걸로 생각하기 -나-
+//            만들어준 Intent 변수에 데이터를 이름표 붙인 상태로 첨부 -강사님-
+            myIntent.putExtra("message",inputMessage)
+
+            startActivity(myIntent)
+       }
     }
 }
